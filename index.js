1bd357a2495290902097be6d6090d3163dc37b99
Artur Krasnyh <arturkrasnyh@gmail.com>
2014-02-08 16:15:47 +0400

    Fix #2: set default file path to cwd

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
 #!/usr/bin/env node
-require('child_process').exec('open -a Brackets ' + require('path').resolve(process.cwd(), process.argv[2]))
+require('child_process').exec('open -a Brackets ' + require('path').resolve(process.cwd(), process.argv[2] || ''))
